e7e5b931bed75077e26c89c707296ef3f398eba0
toufiq007 <t.limon2342@gmail.com>
2022-10-31 16:18:32 +0600

    created a custom slider with js

---
+++ common=concepts-module39/types.js
@@ -0,0 +1,16 @@
+let p = "Javascript";
+let q = p;
+p = "React";
+console.log(q);
+const isTrue = "false";
+if (!isTrue) {
+    console.log("hello");
+} else {
+    console.log("world");
+}
+
+function sum(p, q) {
+    p + q;
+}
+const result = sum(2, 3);
+console.log(result);
